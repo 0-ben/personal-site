1f4815b29e74c8d13501919993586f8920c862ef
0-ben <github@benb.download>
2025-01-17 21:06:46 +0000

    migrate

---
+++ src/lib/components/Email.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let address = ''
+</script>
+
+<a href={`mailto:${address}`}>{address}</a>
+
+<style lang="scss">
+    a {
+        color: greenyellow;
+
+        &:hover, &:active {
+            color: rgb(124, 185, 32);
+        }
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
     import '@fontsource/ubuntu';
     import './main.scss'
+    import Email from '$lib/components/Email.svelte';
 
 </script>
+
+<div id="notice">
+    <h4>If you believe any content of this website to be in violation of any rights you hold, please contact <Email address="admin@benb.my" /></h4>
+</div>
 
 <main style="--height: auto;">
     <slot />
 </main>
 
 <style lang="scss">
+    #notice {
+        text-align: center;
+        height: max-content;
+
+        background-color: #111111aa;
+        padding: 0.5em;
+
+        h4 {
+            margin: 0;
+        }
+    }
     main {
         width: 80vw;
         min-width: 80vw;
